0a4ccd2a44d963521bc7795d1aa312a097cb89c3
irisyangg <ziliny4@nycstudents.net>
2025-05-22 11:17:47 -0400

    Update part2.java
    did part B

--- Q1/part2.java
+++ Q1/part2.java
@@ -1,1 +1,11 @@
-
+ public int moneyCount(int startHour, int endHour){
+        int money=0;
+        for(int i=startHour; i<endHour; i++){
+            int dogs=walkDogs(i);
+            money=money+(dogs*5);
+            if((dogs==maxDogs)||(hours>=9&&<=17)){
+                money=money+3;
+            }
+        }
+        return money;
+    }

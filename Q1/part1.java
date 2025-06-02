4317da663f7ba749ac97613eca1d84d51c38c82c
irisyangg <ziliny4@nycstudents.net>
2025-06-02 18:24:08 -0400

    Update part1.java

--- Q1/part1.java
+++ Q1/part1.java
@@ -1,1 +1,9 @@
-
+public int walkDogs(int hour){
+        int dogs=company.numAvailableDogs(hour);
+        int walked=dogs;
+        if(walked>maxDogs){
+            walked=maxDogs;
+        }
+        company.updateDogs(hour, walked);
+        return walked;
+    }

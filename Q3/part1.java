c771ee4384d70b7f44c33bcbff11bdd7a81dbc94
irisyangg <ziliny4@nycstudents.net>
2025-06-02 19:00:31 -0400

    Update part1.java

--- Q3/part1.java
+++ Q3/part1.java
@@ -1,1 +1,7 @@
-
+public Round(String[] names){
+        competitorList=new ArrayList<>();
+        for(int i=0; i<names.length; i++){
+            Competitor competitor= new Competitor(names[i], i+1);
+            competitorList.add(competitor);
+        }
+    }

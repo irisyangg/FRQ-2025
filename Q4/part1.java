82af01e7927275a7f8ee59f9869e9769ce63a451
irisyangg <ziliny4@nycstudents.net>
2025-06-02 19:07:26 -0400

    Update part1.java

--- Q4/part1.java
+++ Q4/part1.java
@@ -1,1 +1,8 @@
-
+ public sumOrSameGame(int numRows, int numCols){
+        puzzle=new int[numRows][numCols];
+        for(int i=0; i<numRows; i++){
+            for (int j=0; j<numCols; j++){
+                puzzle[r][c]=(int)((Math.random()*9)+1);
+            }
+        }
+    }

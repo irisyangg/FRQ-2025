86370cb53b38bd8105a4ba95ac7b892fd172f060
irisyangg <ziliny4@nycstudents.net>
2025-06-02 19:27:15 -0400

    Update part2.java

--- Q4/part2.java
+++ Q4/part2.java
@@ -1,1 +1,13 @@
-
+public boolean clearPair(int row, int col){
+        int piece=puzzle[row][col];
+        for(int i=row; i<puzzle.length; i++){
+            for(int j=0; j<puzzle[0].length; j++){
+                if(piece==puzzle[i][j] || piece+puzzle[i][j]==10){
+                    puzzle[i][j]=0;
+                    piece=0;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
